21e0eb27af8632fbd03aee4c6e21a5d51194c583
Sachin-kr21 <sachin210302@gmail.com>
2024-04-17 03:12:21 +0530

    adding cicd pipeline

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,13 +1,18 @@
-/* eslint-disable react/prop-types */
-export default function Image(props) {
-  return (
-    // eslint-disable-next-line react/prop-types
-    <div className="relative group">
-    <img className="w-full" src={props.url} alt="Full View" />
-    <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
-        <p className="text-white text-center">View Full Image</p>
-    </div>
-</div>
+import PropTypes from 'prop-types';
 
-  )
+function Image(props) {
+  return (
+    <div className="relative group">
+      <img className="w-full" src={props.url} alt="Full View" />
+      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
+        <p className="text-white text-center">View Full Image</p>
+      </div>
+    </div>
+  );
 }
+
+Image.propTypes = {
+  url: PropTypes.string.isRequired,
+};
+
+export default Image;
