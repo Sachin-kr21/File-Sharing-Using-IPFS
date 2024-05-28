258bd8fdbc8cb1c8bb9ca12251e943d160071660
Sachin-kr21 <sachin210302@gmail.com>
2024-05-29 02:23:17 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
 // import reactRefresh from '@vitejs/plugin-react-refresh'
+import { VitePWA } from "vite-plugin-pwa";
 
 
 dotenv.config();
@@ -12,7 +13,45 @@ export default defineConfig({
   base: "/",
 
 
-  plugins: [react(), 
+  plugins: [react(),VitePWA({
+    devOptions: {
+      enabled: true // For making sure that the PWA is testable from the Local dev environment
+    },
+    registerType: 'autoUpdate',
+    manifest: {
+      name: "Share toh karo",
+      short_name: "Share toh karo",
+      icons: [
+        {
+          "src": "/favicon.ico",
+          "sizes": "64x64 32x32 24x24 16x16",
+          "type": "image/x-icon"
+        },
+        {
+          "src": "/favicon-16x16.png",
+          "type": "image/png",
+          "sizes": "16x16"
+        },
+        {
+          "src": "/favicon-32x32.png",
+          "type": "image/png",
+          "sizes": "32x32"
+        },
+        {
+          "src": "/pwa-192x192.png",
+          "type": "image/png",
+          "sizes": "192x192"
+        },
+        {
+          "src": "/pwa-512x512.png",
+          "type": "image/png",
+          "sizes": "512x512",
+          "purpose": "any maskable" // Icon format that ensures that your PWA icon looks great on all Android devices
+        }
+      ],
+      theme_color: '#AAF',
+    },
+  }), 
     // reactRefresh(),
     sentryVitePlugin({
     org: "wd-s6",
