e267c99661e0a7e7ae48af99213d16176f70b674
Sachin-kr21 <sachin210302@gmail.com>
2024-05-29 04:09:12 +0530

    pwa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Retrieve from "./components/Retrieve.jsx";
 import Upload from "./components/Upload.jsx";
 import Appbar from "./components/Appbar.jsx";
-import "./i18n"
+import "./i18n";
 
 function App() {
   return (
@@ -10,12 +10,12 @@ function App() {
         <Appbar />
       </div>
       <div className="flex h-screen flex-col bg-cover bg-center" style={{ backgroundImage: "url('https://scarlet-adverse-emu-312.mypinata.cloud/ipfs/Qme7w4NXKFrFi73vjdcLzqGUZuefcL1FcGvhF5SqAA3HCp')" }}>
-        <div className="flex p-8 h-screen gap-8">
-          <div className="w-1/2 flex bg-white bg-opacity-80 shadow-lg rounded-lg justify-center p-8 transform transition-transform hover:scale-105">
-            <Upload className="upload-component"/>
+        <div className="flex flex-col md:flex-row p-8 h-screen gap-8">
+          <div className="w-full md:w-1/2 h-1/2 md:h-full flex bg-white bg-opacity-80 shadow-lg rounded-lg justify-center p-8 transform transition-transform hover:scale-105">
+            <Upload className="upload-component" />
           </div>
-          <div className="w-1/2 flex bg-white bg-opacity-80 shadow-lg rounded-lg justify-center p-8 transform transition-transform hover:scale-105">
-            <Retrieve className="retrieve-component"/>
+          <div className="w-full md:w-1/2 h-1/2 md:h-full flex bg-white bg-opacity-80 shadow-lg rounded-lg justify-center p-8 transform transition-transform hover:scale-105">
+            <Retrieve className="retrieve-component" />
           </div>
         </div>
       </div>
